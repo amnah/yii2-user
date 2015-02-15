6e0e6e76384bde095f75e01f57b457b06b086896
amnah <amnah.dev@gmail.com>
2015-02-15 11:14:45 -0500

    update migration file

--- migrations/m150214_044831_init_user.php
+++ migrations/m150214_044831_init_user.php
@@ -1,10 +1,7 @@
 <?php
 
 use amnah\yii2\user\models\User;
-use amnah\yii2\user\models\UserAuth;
-use amnah\yii2\user\models\Profile;
 use amnah\yii2\user\models\Role;
-use amnah\yii2\user\models\UserKey;
 use yii\db\Schema;
 use yii\db\Migration;
 
@@ -18,79 +15,48 @@ class m150214_044831_init_user extends Migration
         }
 
         // create tables. note the specific order
-        $this->createTable(Role::tableName(), [
-            "id" => Schema::TYPE_PK,
-            "name" => Schema::TYPE_STRING . ' not null',
-            "create_time" => Schema::TYPE_TIMESTAMP . ' null default null',
-            "update_time" => Schema::TYPE_TIMESTAMP . ' null default null',
-            "can_admin" => Schema::TYPE_SMALLINT . ' not null default 0',
+        $this->createTable('{{%role}}', [
+            'id' => Schema::TYPE_PK,
+            'name' => Schema::TYPE_STRING . ' not null',
+            'create_time' => Schema::TYPE_TIMESTAMP . ' null default null',
+            'update_time' => Schema::TYPE_TIMESTAMP . ' null default null',
+            'can_admin' => Schema::TYPE_SMALLINT . ' not null default 0',
         ], $tableOptions);
-        $this->createTable(User::tableName(), [
-            "id" => Schema::TYPE_PK,
-            "role_id" => Schema::TYPE_INTEGER . ' not null',
-            "status" => Schema::TYPE_SMALLINT . ' not null',
-            "email" => Schema::TYPE_STRING . ' null default null',
-            "new_email" => Schema::TYPE_STRING . ' null default null',
-            "username" => Schema::TYPE_STRING . ' null default null',
-            "password" => Schema::TYPE_STRING . ' null default null',
-            "auth_key" => Schema::TYPE_STRING . ' null default null',
-            "api_key" => Schema::TYPE_STRING . ' null default null',
-            "login_ip" => Schema::TYPE_STRING . ' null default null',
-            "login_time" => Schema::TYPE_TIMESTAMP . ' null default null',
-            "create_ip" => Schema::TYPE_STRING . ' null default null',
-            "create_time" => Schema::TYPE_TIMESTAMP . ' null default null',
-            "update_time" => Schema::TYPE_TIMESTAMP . ' null default null',
-            "ban_time" => Schema::TYPE_TIMESTAMP . ' null default null',
-            "ban_reason" => Schema::TYPE_STRING . ' null default null',
+        $this->createTable('{{%user}}', [
+            'id' => Schema::TYPE_PK,
+            'role_id' => Schema::TYPE_INTEGER . ' not null',
+            'status' => Schema::TYPE_SMALLINT . ' not null',
+            'email' => Schema::TYPE_STRING . ' null default null',
+            'new_email' => Schema::TYPE_STRING . ' null default null',
+            'username' => Schema::TYPE_STRING . ' null default null',
+            'password' => Schema::TYPE_STRING . ' null default null',
+            'auth_key' => Schema::TYPE_STRING . ' null default null',
+            'api_key' => Schema::TYPE_STRING . ' null default null',
+            'login_ip' => Schema::TYPE_STRING . ' null default null',
+            'login_time' => Schema::TYPE_TIMESTAMP . ' null default null',
+            'create_ip' => Schema::TYPE_STRING . ' null default null',
+            'create_time' => Schema::TYPE_TIMESTAMP . ' null default null',
+            'update_time' => Schema::TYPE_TIMESTAMP . ' null default null',
+            'ban_time' => Schema::TYPE_TIMESTAMP . ' null default null',
+            'ban_reason' => Schema::TYPE_STRING . ' null default null',
         ], $tableOptions);
-        $this->createTable(UserKey::tableName(), [
-            "id" => Schema::TYPE_PK,
-            "user_id" => Schema::TYPE_INTEGER . ' not null',
-            "type" => Schema::TYPE_SMALLINT . ' not null',
-            "key" => Schema::TYPE_STRING . ' not null',
-            "create_time" => Schema::TYPE_TIMESTAMP . ' null default null',
-            "consume_time" => Schema::TYPE_TIMESTAMP . ' null default null',
-            "expire_time" => Schema::TYPE_TIMESTAMP . ' null default null',
+        $this->createTable('{{%user_key}}', [
+            'id' => Schema::TYPE_PK,
+            'user_id' => Schema::TYPE_INTEGER . ' not null',
+            'type' => Schema::TYPE_SMALLINT . ' not null',
+            'key' => Schema::TYPE_STRING . ' not null',
+            'create_time' => Schema::TYPE_TIMESTAMP . ' null default null',
+            'consume_time' => Schema::TYPE_TIMESTAMP . ' null default null',
+            'expire_time' => Schema::TYPE_TIMESTAMP . ' null default null',
         ], $tableOptions);
-        $this->createTable(Profile::tableName(), [
-            "id" => Schema::TYPE_PK,
-            "user_id" => Schema::TYPE_INTEGER . ' not null',
-            "create_time" => Schema::TYPE_TIMESTAMP . ' null default null',
-            "update_time" => Schema::TYPE_TIMESTAMP . ' null default null',
-            "full_name" => Schema::TYPE_STRING . ' null default null',
+        $this->createTable('{{%profile}}', [
+            'id' => Schema::TYPE_PK,
+            'user_id' => Schema::TYPE_INTEGER . ' not null',
+            'create_time' => Schema::TYPE_TIMESTAMP . ' null default null',
+            'update_time' => Schema::TYPE_TIMESTAMP . ' null default null',
+            'full_name' => Schema::TYPE_STRING . ' null default null',
         ], $tableOptions);
-
-        // add indexes for performance optimization
-        $this->createIndex(UserKey::tableName() . "_key", UserKey::tableName(), "key", true);
-        $this->createIndex(User::tableName() . "_email", User::tableName(), "email", true);
-        $this->createIndex(User::tableName() . "_username", User::tableName(), "username", true);
-
-        // add foreign keys for data integrity
-        $this->addForeignKey(User::tableName() . "_role_id", User::tableName(), "role_id", Role::tableName(), "id");
-        $this->addForeignKey(Profile::tableName() . "_user_id", Profile::tableName(), "user_id", User::tableName(), "id");
-        $this->addForeignKey(UserKey::tableName() . "_user_id", UserKey::tableName(), "user_id", User::tableName(), "id");
-
-        // insert role data
-        $columns = ["name", "can_admin", "create_time"];
-        $this->batchInsert(Role::tableName(), $columns, [
-            ["Admin", 1, date("Y-m-d H:i:s")],
-            ["User", 0, date("Y-m-d H:i:s")],
-        ]);
-
-        // insert admin user: neo/neo
-        $security = Yii::$app->security;
-        $columns = ["role_id", "email", "username", "password", "status", "create_time", "api_key", "auth_key"];
-        $this->batchInsert(User::tableName(), $columns, [
-            [Role::ROLE_ADMIN, "neo@neo.com", "neo", '$2y$10$WYB666j7MmxuW6b.kFTOde/eGCLijWa6BFSjAAiiRbSAqpC1HCmrC', User::STATUS_ACTIVE, date("Y-m-d H:i:s"), $security->generateRandomString(), $security->generateRandomString()],
-        ]);
-
-        // insert profile data
-        $columns = ["user_id", "full_name", "create_time"];
-        $this->batchInsert(Profile::tableName(), $columns, [
-            [1, "the one", date("Y-m-d H:i:s")],
-        ]);
-
-        $this->createTable(UserAuth::tableName(), [
+        $this->createTable('{{%user_auth}}', [
             'id' => Schema::TYPE_PK,
             'user_id' => Schema::TYPE_INTEGER . ' not null',
             'provider' => Schema::TYPE_STRING . ' not null',
@@ -101,18 +67,54 @@ class m150214_044831_init_user extends Migration
         ], $tableOptions);
 
         // add indexes for performance optimization
-        $this->createIndex(UserAuth::tableName() . "_provider_id", UserAuth::tableName(), "provider_id", false);
+        $this->createIndex('{{%user_email}}', '{{%user}}', 'email', true);
+        $this->createIndex('{{%user_username}}', '{{%user}}', 'username', true);
+        $this->createIndex('{{%user_key_key}}', '{{%user_key}}', 'key', true);
+        $this->createIndex('{{%user_auth_provider_id}}', '{{%user_auth}}', 'provider_id', false);
 
         // add foreign keys for data integrity
-        $this->addForeignKey(UserAuth::tableName() . "_user_id", UserAuth::tableName(), "user_id", User::tableName(), "id");
+        $this->addForeignKey('{{%user_role_id}}', '{{%user}}', 'role_id', '{{%role}}', 'id');
+        $this->addForeignKey('{{%profile_user_id}}', '{{%profile}}', 'user_id', '{{%user}}', 'id');
+        $this->addForeignKey('{{%user_key_user_id}}', '{{%user_key}}', 'user_id', '{{%user}}', 'id');
+        $this->addForeignKey('{{%user_auth_user_id}}', '{{%user_auth}}', 'user_id', '{{%user}}', 'id');
+
+        // insert role data
+        $columns = ['name', 'can_admin', 'create_time'];
+        $this->batchInsert('{{%role}}', $columns, [
+            ['Admin', 1, date('Y-m-d H:i:s')],
+            ['User', 0, date('Y-m-d H:i:s')],
+        ]);
+
+        // insert admin user: neo/neo
+        $security = Yii::$app->security;
+        $columns = ['role_id', 'email', 'username', 'password', 'status', 'create_time', 'api_key', 'auth_key'];
+        $this->batchInsert('{{%user}}', $columns, [
+            [
+                Role::ROLE_ADMIN,
+                'neo@neo.com',
+                'neo',
+                '$2y$13$dyVw4WkZGkABf2UrGWrhHO4ZmVBv.K4puhOL59Y9jQhIdj63TlV.O',
+                User::STATUS_ACTIVE,
+                date('Y-m-d H:i:s'),
+                $security->generateRandomString(),
+                $security->generateRandomString(),
+            ],
+        ]);
+
+        // insert profile data
+        $columns = ['user_id', 'full_name', 'create_time'];
+        $this->batchInsert('{{%profile}}', $columns, [
+            [1, 'the one', date('Y-m-d H:i:s')],
+        ]);
     }
 
     public function safeDown()
     {
         // drop tables in reverse order (for foreign key constraints)
-        $this->dropTable(Profile::tableName());
-        $this->dropTable(UserKey::tableName());
-        $this->dropTable(User::tableName());
-        $this->dropTable(Role::tableName());
+        $this->dropTable('{{%user_auth}}');
+        $this->dropTable('{{%profile}}');
+        $this->dropTable('{{%user_key}}');
+        $this->dropTable('{{%user}}');
+        $this->dropTable('{{%role}}');
     }
 }
