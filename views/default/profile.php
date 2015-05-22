cf75e30a1da257b5600c89bc303884a9634d8082
amnah <amnah.dev@gmail.com>
2015-05-21 20:00:37 -0400

    replace tabs with 4 spaces

--- views/default/profile.php
+++ views/default/profile.php
@@ -14,7 +14,7 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="user-default-profile">
 
-	<h1><?= Html::encode($this->title) ?></h1>
+    <h1><?= Html::encode($this->title) ?></h1>
 
     <?php if ($flash = Yii::$app->session->getFlash("Profile-success")): ?>
 
