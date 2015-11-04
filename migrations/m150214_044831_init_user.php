2b12945265fbfe56094b553e00c90d83e22fe50a
amnah <amnah.dev@gmail.com>
2015-11-03 22:03:32 -0500

    simplify migration a bit

--- migrations/m150214_044831_init_user.php
+++ migrations/m150214_044831_init_user.php
@@ -16,42 +16,42 @@ class m150214_044831_init_user extends Migration
         $this->createTable('{{%role}}', [
             'id' => Schema::TYPE_PK,
             'name' => Schema::TYPE_STRING . ' not null',
-            'created_at' => Schema::TYPE_TIMESTAMP . ' null default null',
-            'updated_at' => Schema::TYPE_TIMESTAMP . ' null default null',
+            'created_at' => Schema::TYPE_TIMESTAMP . ' null',
+            'updated_at' => Schema::TYPE_TIMESTAMP . ' null',
             'can_admin' => Schema::TYPE_SMALLINT . ' not null default 0',
         ], $tableOptions);
         $this->createTable('{{%user}}', [
             'id' => Schema::TYPE_PK,
             'role_id' => Schema::TYPE_INTEGER . ' not null',
             'status' => Schema::TYPE_SMALLINT . ' not null',
-            'email' => Schema::TYPE_STRING . ' null default null',
-            'username' => Schema::TYPE_STRING . ' null default null',
-            'password' => Schema::TYPE_STRING . ' null default null',
-            'auth_key' => Schema::TYPE_STRING . ' null default null',
-            'access_token' => Schema::TYPE_STRING . ' null default null',
-            'logged_in_ip' => Schema::TYPE_STRING . ' null default null',
-            'logged_in_at' => Schema::TYPE_TIMESTAMP . ' null default null',
-            'created_ip' => Schema::TYPE_STRING . ' null default null',
-            'created_at' => Schema::TYPE_TIMESTAMP . ' null default null',
-            'updated_at' => Schema::TYPE_TIMESTAMP . ' null default null',
-            'banned_at' => Schema::TYPE_TIMESTAMP . ' null default null',
-            'banned_reason' => Schema::TYPE_STRING . ' null default null',
+            'email' => Schema::TYPE_STRING . ' null',
+            'username' => Schema::TYPE_STRING . ' null',
+            'password' => Schema::TYPE_STRING . ' null',
+            'auth_key' => Schema::TYPE_STRING . ' null',
+            'access_token' => Schema::TYPE_STRING . ' null',
+            'logged_in_ip' => Schema::TYPE_STRING . ' null',
+            'logged_in_at' => Schema::TYPE_TIMESTAMP . ' null',
+            'created_ip' => Schema::TYPE_STRING . ' null',
+            'created_at' => Schema::TYPE_TIMESTAMP . ' null',
+            'updated_at' => Schema::TYPE_TIMESTAMP . ' null',
+            'banned_at' => Schema::TYPE_TIMESTAMP . ' null',
+            'banned_reason' => Schema::TYPE_STRING . ' null',
         ], $tableOptions);
         $this->createTable('{{%user_token}}', [
             'id' => Schema::TYPE_PK,
             'user_id' => Schema::TYPE_INTEGER . ' not null',
             'type' => Schema::TYPE_SMALLINT . ' not null',
             'token' => Schema::TYPE_STRING . ' not null',
-            'data' => Schema::TYPE_STRING . ' null default null',
-            'created_at' => Schema::TYPE_TIMESTAMP . ' null default null',
-            'expired_at' => Schema::TYPE_TIMESTAMP . ' null default null',
+            'data' => Schema::TYPE_STRING . ' null',
+            'created_at' => Schema::TYPE_TIMESTAMP . ' null',
+            'expired_at' => Schema::TYPE_TIMESTAMP . ' null',
         ], $tableOptions);
         $this->createTable('{{%profile}}', [
             'id' => Schema::TYPE_PK,
             'user_id' => Schema::TYPE_INTEGER . ' not null',
-            'created_at' => Schema::TYPE_TIMESTAMP . ' null default null',
-            'updated_at' => Schema::TYPE_TIMESTAMP . ' null default null',
-            'full_name' => Schema::TYPE_STRING . ' null default null',
+            'created_at' => Schema::TYPE_TIMESTAMP . ' null',
+            'updated_at' => Schema::TYPE_TIMESTAMP . ' null',
+            'full_name' => Schema::TYPE_STRING . ' null',
         ], $tableOptions);
         $this->createTable('{{%user_auth}}', [
             'id' => Schema::TYPE_PK,
@@ -59,8 +59,8 @@ class m150214_044831_init_user extends Migration
             'provider' => Schema::TYPE_STRING . ' not null',
             'provider_id' => Schema::TYPE_STRING . ' not null',
             'provider_attributes' => Schema::TYPE_TEXT . ' not null',
-            'created_at' => Schema::TYPE_TIMESTAMP . ' null default null',
-            'updated_at' => Schema::TYPE_TIMESTAMP . ' null default null'
+            'created_at' => Schema::TYPE_TIMESTAMP . ' null',
+            'updated_at' => Schema::TYPE_TIMESTAMP . ' null'
         ], $tableOptions);
 
         // add indexes for performance optimization
