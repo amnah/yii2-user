234c2de2810b9fb0882f612142c4bed0c90fc864
amnah <amnah.dev@gmail.com>
2016-01-16 20:38:06 -0500

    add italian language file

---
+++ messages/it/user.php
@@ -0,0 +1,103 @@
+<?php
+
+return [
+
+    // models
+    'ID' => '',
+    'User ID' => 'ID Utente',
+    'Created At' => 'Data di creazione',
+    'Updated At' => 'Data di modifica',
+    'Full Name' => 'Nome completo',
+
+    'Name' => 'Nome',
+    'Can Admin' => 'Può amministrare',
+
+    'Role' => 'Ruolo',
+    'Role ID' => 'ID Ruolo',
+    'Status' => 'Stato',
+    'Email' => 'Email',
+    'New Email' => 'Nuova email',
+    'Username' => 'Nome utente',
+    'Password' => 'Password',
+    'Auth Key' => 'Chiave di autenticazione',
+    'Access Token' => 'Token di accesso',
+    'Logged In Ip' => 'IP di connessione',
+    'Logged In At' => 'Data di connessione',
+    'Created Ip' => 'IP di creazione',
+    'Banned At' => 'Data di divieto',
+    'Banned Reason' => 'Motivo del divieto',
+    'Current Password' => 'Password corrente',
+    'New Password' => 'Nuova password',
+    'New Password Confirm' => 'Conferma della nuova password',
+    'Email Confirmation' => 'Conferma email',
+
+    'Provider' => 'Provider',
+    'Provider ID' => 'ID provider',
+    'Provider Attributes' => 'Attributi del provider',
+
+    'Type' => 'Tipo',
+    'Token' => 'Token',
+    'Expired At' => 'Data di scadenza',
+
+    // models/forms
+    'Email not found' => 'Email non trovata',
+    'Email / Username' => 'Email / Nome utente',
+    'Email / Username not found' => 'Email / Nome utente non trovato',
+    'Username not found' => 'Nome utente non trovato',
+    'User is banned - {banReason}' => 'L\'utente è bannato - {banReason}',
+    'Incorrect password' => 'Password non corretta',
+    'Remember Me' => 'Resta collegato',
+    'Email is already active' => 'Email già esitente',
+    'Passwords do not match' => 'La password non coincide',
+    '{attribute} can contain only letters, numbers, and "_"' => '{attribute} può contenere solo lettere, numeri e "_"',
+
+    // controllers
+    'Successfully registered [ {displayName} ]' => 'Registrato con successo [ {displayName} ]',
+    ' - Please check your email to confirm your account' => ' - Per favore, controlla la tua email per confermare il tuo account',
+    'Account updated' => 'Account modificato',
+    'Profile updated' => 'Profilo modificato',
+    'Confirmation email resent' => 'Conferma email inviata di nuovo',
+    'Email change cancelled' => 'Cambio di email annullato',
+    'Instructions to reset your password have been sent' => 'Le istruzioni per reimpostare la password sono state inviate',
+
+    // mail
+    'Please confirm your email address by clicking the link below:' => 'Per favore, conferma il tuo indirizzo email cliccando sul seguente link:',
+    'Please use this link to reset your password:' => 'Per favore, usa questo link per reimpostare la tua password:',
+
+    // admin views
+    'Users' => 'Utenti',
+    'Banned' => 'Bannato',
+    'Create' => 'Crea',
+    'Update' => 'Modifica',
+    'Delete' => 'Elimina',
+    'Search' => 'Cerca',
+    'Reset' => 'Reset',
+    'Create {modelClass}' => 'Crea {modelClass}',
+    'Update {modelClass}: ' => 'Modifica {modelClass}: ',
+    'Are you sure you want to delete this item?' => 'Sei sicuro di voler eliminare questo elemento?',
+
+    // default views
+    'Account' => 'Account',
+    'Pending email confirmation: [ {newEmail} ]' => 'In attesa di conferma email: [ {newEmail} ]',
+    'Cancel' => 'Annulla',
+    'Changing your email requires email confirmation' => 'La modifica della tua email richiede una conferma via email',
+    'Confirmed' => 'Confermato',
+    'Error' => 'Errore',
+    'Your email [ {email} ] has been confirmed' => 'La tua email [ {email} ] è stata confermata',
+    'Go to my account' => 'Vai al mio account',
+    'Go home' => 'Vai all\'inizio',
+    'Log in here' => 'Accedi qui',
+    'Invalid Token' => 'Token non valido',
+    'Forgot password' => 'Password dimenticata',
+    'Submit' => 'Invia',
+    'Yii 2 User' => 'Utente di Yii 2',
+    'Login' => 'Accedi',
+    'Register' => 'Registrati',
+    'Logout' => 'Esci',
+    'Resend confirmation email' => 'Invia di nuovo email di conferma',
+    'Profile' => 'Profilo',
+    'Resend' => 'Invia di nuovo',
+    'Password has been reset' => 'La password è stata resettata',
+//    'Login link sent - Please check your email' => '',
+//    'Registration link sent - Please check your email' => '',
+];
