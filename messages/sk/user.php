a056eb77d810e97fe6603a974e1b1bb7bf84a5ed
Robert Natkay <robert@nrcode.com>
2016-11-16 12:05:36 +0100

    add slovak translation

---
+++ messages/sk/user.php
@@ -0,0 +1,103 @@
+<?php
+
+return [
+
+    // models
+    'ID' => 'ID',
+    'User ID' => 'ID používateľa',
+    'Created At' => 'Vytvorené',
+    'Updated At' => 'Aktualizované',
+    'Full Name' => 'Celé meno',
+
+    'Name' => 'Meno',
+    'Can Admin' => 'Administrátor',
+
+    'Role' => 'Rola',
+    'Role ID' => 'Role ID',
+    'Status' => 'Stav',
+    'Email' => 'E-mail',
+    'New Email' => 'Nový e-mail',
+    'Username' => 'Používateľské meno',
+    'Password' => 'Heslo',
+//    'Auth Key' => '',
+//    'Access Token' => '',
+    'Logged In Ip' => 'Ip prihlásenia',
+    'Logged In At' => 'Čas prihlásenia',
+    'Created Ip' => 'Ip registacie',
+    'Banned At' => 'Čas blokovania',
+    'Banned Reason' => 'Dôvod blokovania',
+    'Current Password' => 'Aktuálne heslo',
+    'New Password' => 'Nové heslo',
+    'New Password Confirm' => 'Potvrdenie hesla',
+    'Email Confirmation' => 'Potvrdenie E-mail adresy',
+
+    'Provider' => 'Poskytovateľ',
+    'Provider ID' => 'ID poskytovateľa',
+    'Provider Attributes' => 'Atribúty poskytovateľa',
+
+    'Type' => 'Typ',
+//    'Token' => '',
+//    'Expired At' => '',
+
+    // models/forms
+    'Email not found' => 'E-mail nenájdené',
+    'Email / Username' => 'E-mail / prihlasovacie meno',
+    'Email / Username not found' => 'E-mail / prihlasovacie meno nenájdené',
+    'Username not found' => 'Prihlasovacie meno nenájdené',
+    'User is banned - {banReason}' => 'Používateľ je blokovaný - {banReason}',
+    'Incorrect password' => 'Nesprávne heslo',
+    'Remember Me' => 'Zapamätať',
+    'Email is already active' => 'E-mail je už aktívny',
+    'Passwords do not match' => 'Heslá sa nezhodujú',
+    '{attribute} can contain only letters, numbers, and "_"' => '{attribute} môže obsahovať len písmená, čísla a znak "_"',
+
+    // controllers
+    'Successfully registered [ {displayName} ]' => 'Úspešná registrácia [ {displayName} ]',
+    ' - Please check your email to confirm your account' => ' - Na potvrdenie účtu prosím skontrolujte svoju e-mailovú adresu',
+    'Account updated' => 'Účet aktualizovaný',
+    'Profile updated' => 'Profil aktualizovaný',
+    'Confirmation email resent' => 'Potvrdenie bolo poslané znova',
+    'Email change cancelled' => 'Zmena e-mailovej adresy je zrušené',
+    'Instructions to reset your password have been sent' => 'Inštrukcie na obnovu hesla boli zaslané',
+
+    // mail
+    'Please confirm your email address by clicking the link below:' => 'Prosím potvrďte vašu e-mailovú adresu s kliknutím na odkaz nižšie:',
+        'Please use this link to reset your password:' => 'Na obnovu hesla navštívte nasledujúcu adresu',
+
+    // admin views
+    'Users' => 'Používatelia',
+    'Banned' => 'Blokované',
+    'Create' => 'Vytvoriť',
+    'Update' => 'Upraviť',
+    'Delete' => 'Zmazať',
+    'Search' => 'Hladať',
+    'Reset' => 'Resetovať',
+    'Create {modelClass}' => 'Vytvorenie {modelClass}',
+    'Update {modelClass}: ' => 'Úprava {modelClass}',
+    'Are you sure you want to delete this item?' => 'Skutočne to chcete zmazať?',
+
+    // default views
+    'Account' => 'Účet',
+    'Pending email confirmation: [ {newEmail} ]' => 'Potvrdenie čaká na schválenie: [ {newEmail} ]',
+    'Cancel' => 'Zrušiť',
+    'Changing your email requires email confirmation' => 'Na zmenu e-mail adresy je potrebné to potvrdiť',
+    'Confirmed' => 'Schválené',
+    'Error' => 'Chyba',
+    'Your email [ {email} ] has been confirmed' => 'Vaša e-mailová adresa bola schválená',
+    'Go to my account' => 'Na môj účet',
+    'Go home' => 'Domov',
+    'Log in here' => 'Prihlásenie',
+//    'Invalid Token' => '',
+    'Forgot password' => 'Zabudnuté heslo',
+    'Submit' => 'Zaslať',
+    'Yii 2 User' => 'Yii 2 Používateľ',
+    'Login' => 'Prihlásenie',
+    'Register' => 'Registrácia',
+    'Logout' => 'Odhlásenie',
+    'Resend confirmation email' => 'Znovu odoslať e-mail na schválenie',
+    'Profile' => 'Profil',
+    'Resend' => 'Znovu odoslať',
+    'Password has been reset' => 'Vaše heslo bolo obnovené',
+//    'Login link sent - Please check your email' => '',
+//    'Registration link sent - Please check your email' => '',
+];
