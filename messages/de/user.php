716d705f4e269fd7f35218752b70164af4846cab
Falco Kleinschmidt <gitlab@datenfalke.de>
2016-12-07 19:11:01 +0000

    Added translation for German

---
+++ messages/de/user.php
@@ -0,0 +1,103 @@
+<?php
+
+return [
+
+    // models
+    'ID' => '',
+    'User ID' => 'Benutzer ID',
+    'Created At' => 'Erstellt am',
+    'Updated At' => 'Geändert am',
+    'Full Name' => 'Vollständiger Name',
+
+    'Name' => 'Name',
+    'Can Admin' => 'Kann Administrieren',
+
+    'Role' => 'Rolle',
+    'Role ID' => 'Rollen ID',
+    'Status' => 'Status',
+    'Email' => 'E-Mail',
+    'New Email' => 'Neue E-Mail',
+    'Username' => 'Benutzername',
+    'Password' => 'Passwort',
+    'Auth Key' => 'Authentifizierungs Schlüssel',
+    'Access Token' => 'Zugriffs Token',
+    'Logged In Ip' => 'Eingeloggt mit IP',
+    'Logged In At' => 'Eingeloggt seit',
+    'Created Ip' => 'Erstellt von IP',
+    'Banned At' => 'Ausgeschlossen seit',
+    'Banned Reason' => 'Ausgeschlossen weil',
+    'Current Password' => 'Aktuelles Passwort',
+    'New Password' => 'Neues Passwort',
+    'New Password Confirm' => 'Neues Passwort wiederholen',
+    'Email Confirmation' => 'E-Mail Bestätigung',
+
+    'Provider' => 'Provider',
+    'Provider ID' => 'Provider ID',
+    'Provider Attributes' => 'Provider Attribute',
+
+    'Type' => 'Typ',
+    'Token' => 'Token',
+    'Expired At' => 'Läuft aus am',
+
+    // models/forms
+    'Email not found' => 'E-Mail nicht gefunden',
+    'Email / Username' => 'E-Mail / Benutzername',
+    'Email / Username not found' => 'E-Mail / Benutzername nicht gefunden',
+    'Username not found' => 'Benutzername nicht gefunden',
+    'User is banned - {banReason}' => 'Der Benutzer wurde ausgeschlossen - {banReason}',
+    'Incorrect password' => 'Falsches Passwort',
+    'Remember Me' => 'Erinnere Dich an mich',
+    'Email is already active' => 'E-Mail wurde schon aktiviert',
+    'Passwords do not match' => 'Die Passwörter stimmen nicht überein',
+    '{attribute} can contain only letters, numbers, and "_"' => '{attribute} darf nur Buchstaben, Zahlen und "_" enthalten',
+
+    // controllers
+    'Successfully registered [ {displayName} ]' => 'Erfolgreich registriert [ {displayName} ]',
+    ' - Please check your email to confirm your account' => ' - Überprüfen Sie bitte Ihr E-Mail-Postfach, um Ihr Konto zu bestätigen',
+    'Account updated' => 'Konto geändert',
+    'Profile updated' => 'Profil geändert',
+    'Confirmation email resent' => 'Bestätigungs E-Mail erneut gesendet',
+    'Email change cancelled' => 'Änderung der E-Mail-Adresse abgebrochen',
+    'Instructions to reset your password have been sent' => 'Anweisung für das Zurücksetzen Ihres Passworts wurden gesendet',
+
+    // mail
+    'Please confirm your email address by clicking the link below:' => 'Bitte Bestätigen Sie Ihre E-Mail-Adresse, indem Sie unten auf den Link klicken:',
+    'Please use this link to reset your password:' => 'Bitte benutzen Sie den folgenden Link, um Ihr Passwort zurückzusetzen:',
+
+    // admin views
+    'Users' => 'Benutzer',
+    'Banned' => 'Ausgeschlossen',
+    'Create' => 'Erstellt',
+    'Update' => 'Geändert',
+    'Delete' => 'Gelöscht',
+    'Search' => 'Suchen',
+    'Reset' => 'Reset',
+    'Create {modelClass}' => 'Erstelle {modelClass}',
+    'Update {modelClass}: ' => 'Ändere {modelClass}: ',
+    'Are you sure you want to delete this item?' => 'Sind Sie sicher, dass Sie dieses Element löschen wollen?',
+
+    // default views
+    'Account' => 'Konto',
+    'Pending email confirmation: [ {newEmail} ]' => 'Ausstehende E-Mail-Bestätigung: [ {newEmail} ]',
+    'Cancel' => 'Abbrechen',
+    'Changing your email requires email confirmation' => 'Ihre E-Mail-Adresse muss bestätigt worden sein, um sie ändern zu können',
+    'Confirmed' => 'Bestätigt',
+    'Error' => 'Fehler',
+    'Your email [ {email} ] has been confirmed' => 'Ihre E-Mail-Adresse [ {email} ] wurde bestätigt',
+    'Go to my account' => 'Gehe zu meinem Konto',
+    'Go home' => 'Gehe an den Anfang',
+    'Log in here' => 'Melden Sie sich hier an',
+    'Invalid Token' => 'Ungültiges Token',
+    'Forgot password' => 'Passwort vergessen',
+    'Submit' => 'Abschicken',
+    'Yii 2 User' => 'Yii 2 Benutzer',
+    'Login' => 'Anmelden',
+    'Register' => 'Registrieren',
+    'Logout' => 'Abmelden',
+    'Resend confirmation email' => 'E-Mail-Bestätigung erneut verschicken',
+    'Profile' => 'Profil',
+    'Resend' => 'Erneut schicken',
+    'Password has been reset' => 'Das Passwort wurde zurückgesetzt',
+//    'Login link sent - Please check your email' => '',
+//    'Registration link sent - Please check your email' => '',
+];
