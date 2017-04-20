ba61144a8740ffcb8080bcc1447bf4099698dd2d
Pedro Plowman <p2opensource@gmail.com>
2017-04-21 04:52:37 +1000

    v0.2.1

--- models/Role.php
+++ models/Role.php
@@ -20,9 +20,11 @@ use Yii;
  *
  * @property integer $id
  * @property string $name
- * @property string $created_at
- * @property string $updated_at
  * @property integer $can_admin
+ * @property string $created_at
+ * @property integer $created_by
+ * @property string $updated_at
+ * @property integer $updated_by
  *
  * @property User[] $users
  */
@@ -46,6 +48,16 @@ class Role extends \yii\db\ActiveRecord
 	/**
 	 * @inheritdoc
 	 */
+	public function init()
+	{
+		if (!$this->module) {
+			$this->module = Yii::$app->getModule("user");
+		}
+	}
+
+	/**
+	 * @inheritdoc
+	 */
 	public static function tableName()
 	{
 		return '{{%role}}';
@@ -58,8 +70,8 @@ class Role extends \yii\db\ActiveRecord
 	{
 		$rules = [
 			[['name'], 'required'],
-			[['name'], 'string', 'max' => 255],
-			[['can_admin'], 'integer'],
+			[['name'], 'string', 'max' => 32],
+			[['can_admin', 'created_by', 'updated_by'], 'integer'],
 			[['created_at', 'updated_at'], 'safe'],
 		];
 
@@ -83,7 +95,9 @@ class Role extends \yii\db\ActiveRecord
 			'name' => Yii::t('user', 'Name'),
 			'can_admin' => Yii::t('user', 'Can Admin'),
 			'created_at' => Yii::t('user', 'Created At'),
+			'created_by' => 'Created By',
 			'updated_at' => Yii::t('user', 'Updated At'),
+			'updated_by' => 'Updated By',
 		];
 	}
 
@@ -141,225 +155,10 @@ class Role extends \yii\db\ActiveRecord
 
 	/**
 	 * @inheritdoc
-	 */
-	public function init()
-	{
-		if (!$this->module) {
-			$this->module = Yii::$app->getModule("user");
-		}
-	}
-}
-?>
-
-
-<?php // Yii generated
-
-namespace common\models;
-
-use Yii;
-
-/**
- * This is the model class for table "p2m_role".
- *
- * @property integer $id
- * @property string $name
- * @property integer $can_admin
- * @property string $created_at
- * @property integer $created_by
- * @property string $updated_at
- * @property integer $updated_by
- *
- * @property User[] $users
- */
-class Role extends \yii\db\ActiveRecord
-{
-	/**
-	 * @inheritdoc
-	 */
-	public static function tableName()
-	{
-		return 'p2m_role';
-	}
-
-	/**
-	 * @inheritdoc
-	 */
-	public function rules()
-	{
-		return [
-			[['name'], 'required'],
-			[['can_admin', 'created_by', 'updated_by'], 'integer'],
-			[['created_at', 'updated_at'], 'safe'],
-			[['name'], 'string', 'max' => 32],
-		];
-	}
-
-	/**
-	 * @inheritdoc
-	 */
-	public function attributeLabels()
-	{
-		return [
-			'id' => 'ID',
-			'name' => 'Name',
-			'can_admin' => 'Can Admin',
-			'created_at' => 'Created At',
-			'created_by' => 'Created By',
-			'updated_at' => 'Updated At',
-			'updated_by' => 'Updated By',
-		];
-	}
-
-	/**
-	 * @return \yii\db\ActiveQuery
-	 */
-	public function getUsers()
-	{
-		return $this->hasMany(User::className(), ['role_id' => 'id']);
-	}
-
-	/**
-	 * @inheritdoc
 	 * @return RoleQuery the active query used by this AR class.
 	 */
 	public static function find()
 	{
 		return new RoleQuery(get_called_class());
-	}
-}
-?>
-
-
-<?php // amnah
-
-namespace amnah\yii2\user\models;
-
-use Yii;
-use yii\db\ActiveRecord;
-
-/**
- * This is the model class for table "tbl_role".
- *
- * @property integer $id
- * @property string $name
- * @property string $created_at
- * @property string $updated_at
- * @property integer $can_admin
- *
- * @property User[] $users
- */
-class Role extends ActiveRecord
-{
-	/**
-	 * @var int Admin user role
-	 */
-	const ROLE_ADMIN = 1;
-
-	/**
-	 * @var int Default user role
-	 */
-	const ROLE_USER = 2;
-
-	/**
-	 * @var \amnah\yii2\user\Module
-	 */
-	public $module;
-
-	/**
-	 * @inheritdoc
-	 */
-	public function init()
-	{
-		if (!$this->module) {
-			$this->module = Yii::$app->getModule("user");
-		}
-	}
-
-	/**
-	 * @inheritdoc
-	 */
-	public function rules()
-	{
-		$rules = [
-			[['name'], 'required'],
-			[['name'], 'string', 'max' => 255],
-			[['can_admin'], 'integer'],
-		];
-
-		// add can_ rules
-		foreach ($this->attributes() as $attribute) {
-			if (strpos($attribute, 'can_') === 0) {
-				$rules[] = [[$attribute], 'integer'];
-			}
-		}
-
-		return $rules;
-
-	}
-
-	/**
-	 * @inheritdoc
-	 */
-	public function attributeLabels()
-	{
-		return [
-			'id' => Yii::t('user', 'ID'),
-			'name' => Yii::t('user', 'Name'),
-			'created_at' => Yii::t('user', 'Created At'),
-			'updated_at' => Yii::t('user', 'Updated At'),
-			'can_admin' => Yii::t('user', 'Can Admin'),
-		];
-	}
-
-	/**
-	 * @inheritdoc
-	 */
-	public function behaviors()
-	{
-		return [
-			'timestamp' => [
-				'class' => 'yii\behaviors\TimestampBehavior',
-				'value' => function ($event) {
-					return gmdate("Y-m-d H:i:s");
-				},
-			],
-		];
-	}
-
-	/**
-	 * @return \yii\db\ActiveQuery
-	 */
-	public function getUsers()
-	{
-		$user = $this->module->model("User");
-		return $this->hasMany($user::className(), ['role_id' => 'id']);
-	}
-
-	/**
-	 * Check permission
-	 * @param string $permission
-	 * @return bool
-	 */
-	public function checkPermission($permission)
-	{
-		$roleAttribute = "can_{$permission}";
-		return (bool)$this->$roleAttribute;
-	}
-
-	/**
-	 * Get list of roles for creating dropdowns
-	 * @return array
-	 */
-	public static function dropdown()
-	{
-		// get all records from database and generate
-		static $dropdown;
-		if ($dropdown === null) {
-			$models = static::find()->all();
-			foreach ($models as $model) {
-				$dropdown[$model->id] = $model->name;
-			}
-		}
-		return $dropdown;
 	}
 }
--- models/RoleSearch.php
+++ models/RoleSearch.php
@@ -11,93 +11,18 @@
  * @class \p2m\users\models\RoleSearch
  */
 
-namespace common\models;
+namespace p2m\users\models;
 
 use Yii;
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
 
 /**
- * RoleSearch represents the model behind the search form about `common\models\Role`.
+ * RoleSearch represents the model behind the search form about `p2m\users\models\Role`.
  *
  * @see Role
  */
-class RoleSearch extends \common\models\Role
-{
-	/**
-	 * @inheritdoc
-	 */
-	public function rules()
-	{
-		return [
-			[['id', 'can_admin'], 'integer'],
-			[['name', 'created_at', 'updated_at'], 'safe'],
-		];
-	}
-
-	/**
-	 * @inheritdoc
-	 */
-	public function scenarios()
-	{
-		// bypass scenarios() implementation in the parent class
-		return Model::scenarios();
-	}
-
-	/**
-	 * Creates data provider instance with search query applied
-	 *
-	 * @param array $params
-	 *
-	 * @return ActiveDataProvider
-	 */
-	public function search($params)
-	{
-		$query = Role::find();
-
-		// add conditions that should always apply here
-
-		$dataProvider = new ActiveDataProvider([
-			'query' => $query,
-		]);
-
-		$this->load($params);
-
-		if (!$this->validate()) {
-			// uncomment the following line if you do not want to return any records when validation fails
-			// $query->where('0=1');
-			return $dataProvider;
-		}
-
-		// grid filtering conditions
-		$query->andFilterWhere([
-			'id' => $this->id,
-			'can_admin' => $this->can_admin,
-			'created_at' => $this->created_at,
-			'updated_at' => $this->updated_at,
-		]);
-
-		$query->andFilterWhere(['like', 'name', $this->name]);
-
-		return $dataProvider;
-	}
-}
-?>
-
-
-<?php // Yii generated
-
-namespace common\models;
-
-use Yii;
-use yii\base\Model;
-use yii\data\ActiveDataProvider;
-use common\models\Role;
-
-/**
- * RoleSearch represents the model behind the search form about `common\models\Role`.
- */
-class RoleSearch extends Role
+class RoleSearch extends \p2m\users\models\Role
 {
 	/**
 	 * @inheritdoc
