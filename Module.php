e560965735d0fb4fd28efbd58d8d2107df81b5b8
amnah <amnah@users.noreply.github.com>
2017-10-15 13:49:21 -0400

    update to version 5.0.8

--- Module.php
+++ Module.php
@@ -16,7 +16,7 @@ class Module extends \yii\base\Module
     /**
      * @var string Module version
      */
-    protected $version = "5.0.7";
+    protected $version = "5.0.8";
 
     /**
      * @var string Alias for module
